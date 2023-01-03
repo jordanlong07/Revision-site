9dc47dc6808cddb0cc7461c75edfe6c1b2454d11
jordanlong07 <70574837+jordanlong07@users.noreply.github.com>
2023-01-03 15:53:54 +0000

    adding anlytics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang='ts'>
    // Homepage
    import "../app.css";
-
+   import { inject } from "@vercel/analytics"
+   inject()
 </script>
 <div>
    <p class='text-center mt-4'>Hello and welcome to the OS revision page. Here we will have quick questions to help you revise for this module. Feel free to upload any you have above!</p>
